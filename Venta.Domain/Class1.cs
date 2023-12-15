3abcfae3a71da055d24edaae5852868952a6e7f9
Gustavo Cruzado <gustavo_149210@hotmail.com>
2023-12-14 21:41:21 -0500

    git ignore

---
+++ Venta.Domain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Venta.Domain
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Ventas.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Ventas.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
